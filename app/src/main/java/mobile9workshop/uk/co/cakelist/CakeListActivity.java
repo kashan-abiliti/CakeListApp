906bb8167975c13c5811aad298968988e78430e6
Kashan Qamar <kqamar@abiliti.co>
2019-11-22 16:25:45 +0000

    base commit - project

---
+++ app/src/main/java/mobile9workshop/uk/co/cakelist/CakeListActivity.java
@@ -0,0 +1,14 @@
+package mobile9workshop.uk.co.cakelist;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CakeListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cake_list);
+    }
+}
