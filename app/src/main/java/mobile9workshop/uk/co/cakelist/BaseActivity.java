74f3dd6f7f6ccf66aeb0955f1f28340dc6d130af
Kashan Qamar <kqamar@abiliti.co>
2019-11-25 21:18:28 +0000

    gradle setup

---
+++ app/src/main/java/mobile9workshop/uk/co/cakelist/BaseActivity.java
@@ -0,0 +1,37 @@
+package mobile9workshop.uk.co.cakelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.jetbrains.annotations.Nullable;
+
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // call this method to restart app
+    protected void restartApplication(){
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+}
---
+++ app/src/main/java/mobile9workshop/uk/co/cakelist/CakeListActivity.kt
@@ -0,0 +1,14 @@
+package mobile9workshop.uk.co.cakelist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CakeListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_cake_list)
+
+    }
+}
